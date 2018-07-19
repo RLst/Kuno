1f1552a572826f0a39975e25f440eeb62d74b9b0
Tony Le <letony@icloud.com>
2018-07-19 13:49:24 +1000

    Basic pathfinding classes and structures set up

--- Kuno/PF.cpp
+++ Kuno/PF.cpp
@@ -1,2 +1,37 @@
 #include "PF.h"
-//For now do definitions in header file to reduce complexity
+
+namespace PF {
+
+PF::Node * PF::Graph::addNode(pkr::Vector2 pos)
+{
+	//Create new node with set properties, push it onto graph node list and return
+	Node* newNode = new Node(pos);
+	m_nodes.push_back(newNode);
+	return newNode;
+}
+
+void PF::Graph::addConnection(Node * nodeFrom, Node * nodeTo, float cost)
+{
+	Node::connect(nodeFrom, nodeTo, cost);
+	//Edge* edge = new Edge(nodeTo, cost);			//Create new edge and connect to TARGET node and set costs
+	//nodeFrom->connections.push_back(edge);			//Connect edge to RECEIVING node
+}
+
+void PF::Graph::draw(aie::Renderer2D * renderer)
+{
+	for (auto node : m_nodes) {
+		for (auto it : node->connections)
+		{
+			//Draw the edges
+			Edge* edge = it;
+			renderer->setRenderColour(1, 1, 1);
+			renderer->drawLine(node->pos.x, node->pos.y, edge->connectTo->pos.x, edge->connectTo->pos.y);
+		}
+		//draw the nodes
+		renderer->setRenderColour(1, 0, 0);		//Required?
+												//renderer->drawBox(node->pos.x, node->pos.y, 10, 10);
+		renderer->drawCircle(node->pos.x, node->pos.y, 10);
+	}
+}
+
+}
--- Kuno/PF.h
+++ Kuno/PF.h
@@ -1,7 +1,80 @@
 #pragma once
 
+#include <Vector2.h>
+#include <vector>
+#include <list>
+#include <Renderer2D.h>
+
 //Pathfinding related methods and implementations
 namespace PF {
 
+	struct Node;
+
+	struct Edge
+	{
+		Node*	connectTo;
+		float	cost;
+
+		Edge() : connectTo(nullptr), cost(0) {}		//Default
+
+		Edge(Node* nodeTo) : connectTo(nodeTo), cost(0) {}		//Overload (zero cost)
+
+		Edge(Node* nodeTo, float cost) : connectTo(nodeTo), cost(cost) {}		//Overload
+	};
+
+	struct Node
+	{
+		Node*				parent;
+		float				gScore;
+		pkr::Vector2		pos;
+		std::vector<Edge*>	connections;
+
+		Node() : gScore(0), pos(0, 0), parent(nullptr) {}		//Default
+
+		Node(pkr::Vector2 pos) : gScore(0), pos(pos), parent(nullptr) {}		//Overload (nullptr parent)
+
+		Node(pkr::Vector2 pos, Node* parent) : gScore(0), pos(pos), parent(parent) {}		//Overload
+
+																							//Set connection (helper function?)
+		void static connect(Node *nodeFrom, Node *nodeTo, float cost) {
+			//Create new edge and connect to TARGET node and set costs
+			Edge* edge = new Edge(nodeTo, cost);
+			//Connect edge to RECEIVING node
+			nodeFrom->connections.push_back(edge);
+		}
+	};
+
+	class Graph
+	{
+	protected:
+		std::list<Node*>	m_nodes;		//The graph's container of nodes; should I use list or vectors?
+		aie::Texture*		m_texture;
+
+	public:
+		Graph() {};
+		~Graph() {			//Delete all nodes
+			for (auto &node : m_nodes) {
+				delete node;
+				node = nullptr;
+			}
+		}
+
+		Node*	addNode(pkr::Vector2 pos);
+
+		//Remove the given node
+		void	removeNode(Node* node) { m_nodes.remove(node); }
+
+		void	addConnection(Node* nodeFrom, Node* nodeTo, float cost = 1.0f);
+
+		void	draw(aie::Renderer2D* renderer);
+
+		std::list<Node*>	AStarSearch(Node* startNode, Node* endNode);		//Returns A* path
+
+		std::list<Node*>	DjikstraSearch(Node* startNode, Node* endNode);		//Returns djikstra path
+
+		void	findNodes(std::list<Node*> &list, pkr::Vector2 position, float distance);	//do I need this?
+		void	update(float deltaTime);		//Don't think this is needed
+		//void	findPathDijkstra(Node* nodeStart, const std::list<Node*> &endNodes, std::list<Node*> &outPath);
+	};
 }
 

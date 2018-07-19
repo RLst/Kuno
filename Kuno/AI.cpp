e74135cc9d7492cf2e8d0262de8c32be5e1a3b1d
Tony Le <letony@icloud.com>
2018-07-19 16:57:22 +1000

    Basic AI functionality implementated (Behaviour tree)

--- Kuno/AI.cpp
+++ Kuno/AI.cpp
@@ -1,2 +1,132 @@
 #include "AI.h"
-//For now do definitions in header file to reduce complexity
+
+#include <pkr\Vector2.h>
+#include <Renderer2D.h>
+
+namespace AI {
+
+	eResult Selector::execute(Agent * agent, float deltaTime)
+	{
+		//OR node; Returns SUCCESS if any child returns SUCCESS
+		for (auto child : m_childBehaviours) {
+			if (child->execute(agent, deltaTime) == eResult::SUCCESS)
+				return eResult::SUCCESS;
+		}
+		return eResult::FAILURE;
+	}
+
+	eResult Sequence::execute(Agent * agent, float deltaTime)
+	{
+		//AND node; Returns SUCCESS only if ALL children return SUCCESS
+		for (auto child : m_childBehaviours) {
+			if (child->execute(agent, deltaTime) == eResult::FAILURE)
+				return eResult::FAILURE;
+		}
+		return eResult::SUCCESS;
+	}
+
+	Agent::Agent(const Agent & other) :
+		m_behaviours(other.m_behaviours),
+		m_pos(other.m_pos),
+		m_size(other.m_size),
+		m_colour(other.m_colour)
+	{}
+
+	Agent::Agent(const pkr::Vector2 & pos, float size, const pkr::Vector3 & colour) :
+		m_pos(pos), m_size(size), m_colour(colour)
+	{}
+
+	Agent::~Agent()
+	{
+		for (auto &behaviour : m_behaviours) {
+			delete behaviour;			//Do the behaviours have to be deleted?
+			behaviour = nullptr;
+		}
+	}
+
+	void Agent::addBehaviour(iBehaviour * behaviour)
+	{
+		m_behaviours.push_back(behaviour);
+	}
+
+	void Agent::addForce(const pkr::Vector2 & force)
+	{
+		m_force += force;
+	}
+
+	void Agent::update(float deltaTime)
+	{
+		//Execute all behaviours
+		for (auto behaviour : m_behaviours) {
+			behaviour->execute(this, deltaTime);
+		}
+	}
+
+	void Agent::draw(aie::Renderer2D * renderer)
+	{
+		//Draw agent as a coloured circle
+		renderer->setRenderColour(m_colour.r, m_colour.g, m_colour.b);
+		renderer->drawCircle(m_pos.x, m_pos.y, m_size);
+	}
+
+	eResult tEnemiesCloseCondition::execute(Agent * agent, float deltaTime)
+	{
+		//Return success if an enemy is within range
+		for (auto enemy : m_enemies) {
+			if (pkr::Vector2::distance(agent->getPos(), enemy->getPos()) < m_range) {
+				//Retrieve the enemy that is in range
+				m_enemyFoundInRange = enemy;
+				return eResult::SUCCESS;
+			}
+		}
+		return eResult::FAILURE;
+	}
+
+	eResult tAttackAction::execute(Agent * agent, float deltaTime)
+	{
+		//For each enemy within range of agent
+		for (auto enemy : m_enemiesInRange) {
+
+			//Deal damage to enemy
+
+
+			if ()
+		}
+
+		return eResult::SUCCESS;
+	}
+
+
+	eResult BasicKeyboardController::execute(Agent * agent, float deltaTime)
+	{
+		//Control this agent
+		//int mouseX = input->getMouseX();
+		//int mouseY = input->getMouseY();
+
+		if (m_input->isKeyDown(aie::INPUT_KEY_W))
+		{
+			agent->addForce(pkr::Vector2(0.0f, 100.0f));
+		}
+		if (m_input->isKeyDown(aie::INPUT_KEY_S))
+		{
+			agent->addForce(pkr::Vector2(0.0f, -100.0f));
+		}
+		if (m_input->isKeyDown(aie::INPUT_KEY_A))
+		{
+			agent->addForce(pkr::Vector2(-100.0f, 0.0f));
+		}
+		if (m_input->isKeyDown(aie::INPUT_KEY_D))
+		{
+			agent->addForce(pkr::Vector2(100.0f, 0.0f));
+		}
+		//agent->rotation = -mouseX * deltaTime;
+
+		return eResult::SUCCESS;
+	}
+
+	eResult SeekAction::execute(Agent * agent, float deltaTime)
+	{
+		return eResult();
+	}
+
+}
--- Kuno/AI.h
+++ Kuno/AI.h
@@ -1,7 +1,169 @@
 #pragma once
 
+#include <vector>
+#include <list>
+#include <pkr\Vector3.h>
+#include <pkr\Vector2.h>
+
+//For header definitions
+#include <Renderer2D.h>
+#include <Input.h>
+
+namespace aie {
+	class Renderer2D;
+	class Font;
+	class Texture;
+	//class Input;
+}
+
 //AI methods and implementations ie. Behaviour tree
 namespace AI {
+
+	class Agent;
+
+	////Core Behaviour Tree Implementation
+	enum eResult {
+		FAILURE,
+		SUCCESS,
+		RUNNING,
+	};
+
+	class iBehaviour 
+	{
+	public:
+		iBehaviour() {}
+		virtual ~iBehaviour() {}	//CHECK how these are supposed to be setup
+		virtual eResult execute(Agent* agent, float deltaTime) = 0;
+	};
+
+	//Composites
+	class iComposite : public iBehaviour
+	{
+	protected:
+		std::list<iBehaviour*>		m_childBehaviours;
+	public:
+		iComposite() {}
+		virtual ~iComposite() {}
+		virtual eResult execute(Agent* agent, float deltaTime) = 0;
+	};
+
+	class Selector : public iComposite
+	{
+	public:
+		Selector() {}
+		~Selector() override {}
+		eResult execute(Agent* agent, float deltaTime) override;
+	};
+
+	class Sequence : public iComposite
+	{
+	public:
+		Sequence() {}
+		~Sequence() override {}
+		eResult execute(Agent* agent, float deltaTime) override;
+	};
+
+	//Decorators
+	class Decorator : public iBehaviour
+	{
+	public:
+		Decorator() {}
+		~Decorator() override {}
+		eResult execute(Agent* agent, float deltaTime) override;
+	};
+
+	////Agents
+	class Agent 
+	{
+	protected:
+		std::vector<iBehaviour*>	m_behaviours;
+
+		//Some basic transformation data
+		pkr::Vector2				m_force;
+		pkr::Vector2				m_accel;
+		pkr::Vector2				m_vel;
+		pkr::Vector2				m_pos;
+		float						m_size;
+		pkr::Vector3				m_colour;
+
+	public:
+		Agent() {}
+		Agent(const Agent &other);		//Copy
+
+		Agent(	const pkr::Vector2 &pos,	//Agent drawn as circle
+				float size,
+				const pkr::Vector3 &colour = pkr::Vector3());
+
+		virtual ~Agent();	//Destructor 
+
+		//Add
+		void		addBehaviour(iBehaviour* behaviour);
+
+		void		addForce(const pkr::Vector2 &force);
+
+		//State accessors
+		pkr::Vector2		getPos() const { return m_pos; }
+
+		//Core
+		void		update(float deltaTime);
+		void		draw(aie::Renderer2D* renderer);
+	};
+
+	///////////////////////////////////////////////////
+	//// Leaf nodes - Where all the action takes place!
+	///////////////////////////////////////////////////
+	//Conditions
+	class tEnemiesCloseCondition : public iBehaviour
+	{
+	private:
+		std::vector<Agent*> m_enemies;
+		float		m_range;
+
+		Agent*		m_enemyInRange;		//How do you relay this information to AttackAction?
+	public:
+		//This class will need to take in a list of enemy agents
+		tEnemiesCloseCondition(std::vector<Agent*> listOfEnemies, float range) :
+			m_enemies(listOfEnemies), m_range(range) {}
+
+		//void setEnemyList(std::vector<Agent*> listOfEnemies);
+		//std::vector<Agent*> getEnemyList() const;
+		eResult execute(Agent* agent, float deltaTime) override;
+	};
+
+	//Actions
+	class tAttackAction : public iBehaviour
+	{
+	private:
+		std::vector<Agent*> m_enemiesInRange;
+		Agent*		m_enemyInRange;
+		int			m_damageToApply;
+		float		m_range;
+
+	public:
+		eResult execute(Agent* agent, float deltaTime) override;
+	};
+
+	class BasicKeyboardController : public iBehaviour
+	{
+	private:
+		aie::Input* m_input;
+	public:
+		BasicKeyboardController(aie::Input * input = aie::Input::getInstance()) : m_input(input) {}
+		eResult execute(Agent* agent, float deltaTime) override;
+	};
+
+	class SeekAction : public iBehaviour 
+		//This needs to take in the target agent
+	{
+	private:
+		Agent *		m_target;
+		float		m_maxSpeed;
+	public:
+		SeekAction(Agent* target, float maxSpeed = 200.0f) :	//Comprehensive
+			m_target(target), m_maxSpeed(maxSpeed) {}
+		~SeekAction() { delete m_target; }						//Destructor
+		eResult execute(Agent* agent, float deltaTime) override;
+	};
 
 }
 

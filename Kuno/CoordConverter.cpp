31642da4cfe4091d045d25e93025e9d132d21acd
Tony Le <letony@icloud.com>
2018-07-27 16:14:08 +1000

    CoordConverter function implementations

--- Kuno/CoordConverter.cpp
+++ Kuno/CoordConverter.cpp
@@ -10,7 +10,7 @@ namespace util {
 		m_cam(camera)
 	{}
 
-	pkr::Vector2 CoordConverter::ViewportToCanvas(float viewportX, float viewportY)
+	pkr::Vector2 CoordConverter::ViewportToCartesian(pkr::Vector2 viewport) const
 	{
 		//Init vars for return
 		pkr::Vector2 canvas = { 0,0 };
@@ -18,9 +18,10 @@ namespace util {
 		auto windowWidth = KunoApp::getInstance()->getWindowWidth();
 		auto windowHeight = KunoApp::getInstance()->getWindowHeight();
 
-		//Borrowed code
-		float xPcentage = viewportX / windowWidth;
-		float yPcentage = viewportY / windowHeight;
+		/// Borrowed code ///
+		//This takes into account the zoom of the camera
+		float xPcentage = viewport.x / windowWidth;
+		float yPcentage = viewport.y / windowHeight;
 
 		float scaledWidth = windowWidth * m_cam->scale;
 		float scaledHeight = windowHeight * m_cam->scale;
@@ -33,7 +34,62 @@ namespace util {
 
 		canvas.x = left + (xPcentage * scaledWidth);
 		canvas.y = bottom + (yPcentage * scaledHeight);
+		/// Borrowed code ///
 
 		return canvas;
 	}
+
+	pkr::Vector2 CoordConverter::ViewportToCartesian(float viewportX, float viewportY) const
+	{
+		//Just redirect
+		return ViewportToCartesian(pkr::Vector2(viewportX, viewportY));
+
+		////Init vars for return
+		//pkr::Vector2 canvas = { 0,0 };
+
+		//auto windowWidth = KunoApp::getInstance()->getWindowWidth();
+		//auto windowHeight = KunoApp::getInstance()->getWindowHeight();
+
+		///// Borrowed code ///
+		//float xPcentage = viewportX / windowWidth;
+		//float yPcentage = viewportY / windowHeight;
+
+		//float scaledWidth = windowWidth * m_cam->scale;
+		//float scaledHeight = windowHeight * m_cam->scale;
+
+		//float midX = m_cam->x + (windowWidth / 2.0f);
+		//float midY = m_cam->y + (windowHeight / 2.0f);
+
+		//float left = midX - (scaledWidth / 2.0f);
+		//float bottom = midY - (scaledHeight / 2.0f);
+
+		//canvas.x = left + (xPcentage * scaledWidth);
+		//canvas.y = bottom + (yPcentage * scaledHeight);
+		///// Borrowed code ///
+
+		//return canvas;
+	}
+
+	pkr::Vector2 CoordConverter::CartesianToIsometric(pkr::Vector2 canvas) const
+	{
+		//// WAIT WHAT??? Is CANVAS actually just CARTESIAN
+		pkr::Vector2 iso = { 0,0 };
+		iso.x = canvas.x - canvas.y;
+		iso.y = (canvas.x + canvas.y) / TILE_RATIO;
+		return iso;
+	}
+
+	pkr::Vector2 CoordConverter::CartesianToIsometric(float canvasX, float canvasY) const
+	{
+		return CartesianToIsometric(pkr::Vector2(canvasX, canvasY));		//Redirect
+	}
+
+	pkr::Vector2 CoordConverter::IsometricToCartesian(pkr::Vector2 iso) const
+	{
+		pkr::Vector2 cart;
+		cart.x = (TILE_RATIO * iso.y + iso.x) / TILE_RATIO;
+		cart.y = (TILE_RATIO * iso.y - iso.x) / TILE_RATIO;
+		return cart;
+	}
+
 }
--- Kuno/CoordConverter.h
+++ Kuno/CoordConverter.h
@@ -14,26 +14,28 @@ class CoordConverter
 private:
 	Camera*		m_cam;
 
-
 public:
 	CoordConverter() = default;
 	~CoordConverter() = default;
 	CoordConverter(Camera* camera = KunoApp::getInstance()->getCamera());		//Auto get ptr to camera
 
 	//// MAIN CONVERSION FUNCTIONS ////
-	//Viewport -> Canvas -> Isometric -> Cartesian
-	static pkr::Vector2		ViewportToCartesian(pkr::Vector2 vpPoint);			//Convert from viewport to cartesian world
-	static pkr::Vector2		ViewportToCartesian(float vpX, float vpY);			
+	pkr::Vector2		ViewportToCartesian(pkr::Vector2 vpPos) const;					//Use by the mouse			
+	pkr::Vector2		ViewportToCartesian(float vpX, float vpY) const;
 
-	//Cartesian -> Isometric
-	static pkr::Vector2		CartesianToIsometric(pkr::Vector2 cart);			//Convert from cartesian world coords to isometric world coords
-	static pkr::Vector2		CartesianToIsometric(float cartX, float cartY);
+	pkr::Vector2		CartesianToIsometric(pkr::Vector2 canvas) const;				//Use by the renderer
+	pkr::Vector2		CartesianToIsometric(float canvasX, float canvasY) const;
 
 private:
 	//// HELPER CONVERSION FUNCTIONS ////
-	static pkr::Vector2		ViewportToCanvas(float viewportX, float viewportY);		//Returns the equivalent canvas coords
-	static pkr::Vector2		CanvasToIsometric(pkr::Vector2 canvas);
-	static pkr::Vector2		IsometricToCartesian(pkr::Vector2 iso);
+	pkr::Vector2		IsometricToCartesian(pkr::Vector2 iso) const;					//Not really required
+
+	//Viewport -> Canvas -> Isometric -> Cartesian
+	//pkr::Vector2		ViewportToCartesian(pkr::Vector2 vpPoint);			//Convert from viewport to cartesian world
+	//pkr::Vector2		ViewportToCartesian(float vpX, float vpY);			
+	//Cartesian -> Isometric
+	//pkr::Vector2		CartesianToIsometric(pkr::Vector2 cart);			//Convert from cartesian world coords to isometric world coords
+	//pkr::Vector2		CartesianToIsometric(float cartX, float cartY);
 };
 
 }

a14637b61768df2c6a8498299fb6c61ade7cd3f8
Tony Le <letony@icloud.com>
2018-07-27 16:55:00 +1000

    CoordConvert constructor cannot take in a singleton as a parameter

--- Kuno/CoordConverter.h
+++ Kuno/CoordConverter.h
@@ -1,5 +1,4 @@
 #pragma once
-#include "KunoApp.h"
 
 namespace pkr {
 	class Vector2;
@@ -7,35 +6,33 @@ namespace pkr {
 
 namespace util {
 
-class Camera;
+	class Camera;
 
-class CoordConverter
-{
-private:
-	Camera*		m_cam;
+	class CoordConverter
+	{
+	private:
+		Camera*		m_cam;
 
-public:
-	CoordConverter() = default;
-	~CoordConverter() = default;
-	CoordConverter(Camera* camera = KunoApp::getInstance()->getCamera());		//Auto get ptr to camera
+		//// HELPER CONVERSION FUNCTIONS ////
+		pkr::Vector2		IsometricToCartesian(pkr::Vector2 iso) const;					//Not really required
 
-	//// MAIN CONVERSION FUNCTIONS ////
-	pkr::Vector2		ViewportToCartesian(pkr::Vector2 vpPos) const;					//Use by the mouse			
-	pkr::Vector2		ViewportToCartesian(float vpX, float vpY) const;
+		//Viewport -> Canvas -> Isometric -> Cartesian
+		//pkr::Vector2		ViewportToCartesian(pkr::Vector2 vpPoint);			//Convert from viewport to cartesian world
+		//pkr::Vector2		ViewportToCartesian(float vpX, float vpY);			
+		//Cartesian -> Isometric
+		//pkr::Vector2		CartesianToIsometric(pkr::Vector2 cart);			//Convert from cartesian world coords to isometric world coords
+		//pkr::Vector2		CartesianToIsometric(float cartX, float cartY);
+	public:
+		CoordConverter() = default;
+		~CoordConverter() = default;
+		CoordConverter(Camera* camera);		//Auto get ptr to camera/// apparently camera can't be default to KunoApp::getInstance()->getCamera()....
 
-	pkr::Vector2		CartesianToIsometric(pkr::Vector2 canvas) const;				//Use by the renderer
-	pkr::Vector2		CartesianToIsometric(float canvasX, float canvasY) const;
+		//// MAIN CONVERSION FUNCTIONS ////
+		pkr::Vector2		ViewportToCartesian(pkr::Vector2 vpPos) const;					//Use by the mouse			
+		pkr::Vector2		ViewportToCartesian(float vpX, float vpY) const;
 
-private:
-	//// HELPER CONVERSION FUNCTIONS ////
-	pkr::Vector2		IsometricToCartesian(pkr::Vector2 iso) const;					//Not really required
-
-	//Viewport -> Canvas -> Isometric -> Cartesian
-	//pkr::Vector2		ViewportToCartesian(pkr::Vector2 vpPoint);			//Convert from viewport to cartesian world
-	//pkr::Vector2		ViewportToCartesian(float vpX, float vpY);			
-	//Cartesian -> Isometric
-	//pkr::Vector2		CartesianToIsometric(pkr::Vector2 cart);			//Convert from cartesian world coords to isometric world coords
-	//pkr::Vector2		CartesianToIsometric(float cartX, float cartY);
-};
+		pkr::Vector2		CartesianToIsometric(pkr::Vector2 canvas) const;				//Use by the renderer
+		pkr::Vector2		CartesianToIsometric(float canvasX, float canvasY) const;
+	};
 
 }

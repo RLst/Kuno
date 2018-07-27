86ea632f43439f19ee27020a72a1c29b9b7f2348
Tony Le <letony@icloud.com>
2018-07-27 15:15:09 +1000

    Tile refined

--- Kuno/Tile.cpp
+++ Kuno/Tile.cpp
@@ -1,9 +1,25 @@
 #include "Tile.h"
+#include "KunoApp.h"
 
 namespace pf {
 
-	Tile::Tile(aie::Texture * texture) :
-		m_texture(texture)
+	Tile::Tile(pkr::Vector2 pos, aie::Texture * tex, pkr::Vector2 texOffset, eTileType type) :
+		m_pos(pos), m_tex(tex), m_texOffset(texOffset), m_type(type)
 	{
 	}
+
+	void Tile::draw(aie::Renderer2D * renderer)
+	{
+		//Calc z buffer depth
+		auto depth = KunoApp::getInstance()->getDepthSorter()->getSortDepth(m_pos.y);
+
+		//Draw
+		renderer->drawSprite(m_tex, m_pos.x, m_pos.y, 0, 0, 0, depth);
+	}
+
+
+
+
+
+
 }
--- Kuno/Tile.h
+++ Kuno/Tile.h
@@ -17,33 +17,46 @@ enum eTileType
 
 class Tile : public Node
 {
+	//// THE IDEAL TILE ////
+	//Iso Tile Width: 100
+	//Iso Tile Height: 50
+	//Image width: 100
+	//Image height: 200
+	//
+
 protected:	
 	//Texture properties
-	aie::Texture*	m_texture;
+	aie::Texture*	m_tex = nullptr;
+	pkr::Vector2	m_texOffset;
 
 	//Tile properties
 	eTileType		m_type;
+	//float			m_width;
+	//float			m_height;
 
-	//Draw...
+	//Cartesian World
+	pkr::Vector2	m_pos;
+	eTileType		m_type;
+
 public:
 	///////////////////////
-	//PUBLIC VARS
-	//Array indices
-	int				X, Y;
-	//pkr::Vector2	offset;
-	//pkr::Vector2	pos;		//Position info for tiles only
-	///////////////////////////
+	////PUBLIC VARS
+	////Array indices
+	//int				X, Y;
+	/////////////////////////////
 
 	Tile() = default;
-	Tile(aie::Texture* texture);
 	virtual ~Tile() = default;	//Texture manager will delete the textures
-	//Tile(int xIndex, int yIndex, pkr::Vector2 pos);
+
+	Tile(pkr::Vector2 position, aie::Texture* tex, pkr::Vector2 texOffset = { 0,0 }, eTileType type = ACCESSIBLE);
 
 	//Accessors
-	aie::Texture*	getTexture() const { return m_texture; }
+	aie::Texture*	getTexture() const { return m_tex; }
+	pkr::Vector2	getTexOffset() const { return m_texOffset; }
+	pkr::Vector2	getPosition() const { return m_pos; }
 
 	//Cores
-	//void			draw(aie::Renderer2D* renderer);
+	void			draw(aie::Renderer2D* renderer);
 };
 
 }

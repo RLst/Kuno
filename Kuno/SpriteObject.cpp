84121dc99b2429d598c44154e537e8b7e9f82ae0
Tony Le <letony@icloud.com>
2018-07-29 21:28:05 +1000

    Sprite; animation system now cancelled. Use setUVRect() instead

--- Kuno/SpriteObject.cpp
+++ Kuno/SpriteObject.cpp
@@ -4,9 +4,10 @@
 namespace ai {
 
 
-SpriteObject::SpriteObject(aie::Texture * texture)
+SpriteObject::SpriteObject(aie::Texture * tex) :
+	m_tex(tex)
 {
-	m_animation.push_back(texture);
+	//m_animation.push_back(texture);
 }
 
 SpriteObject::~SpriteObject()
@@ -14,15 +15,15 @@ SpriteObject::~SpriteObject()
 	//Texture manager should be responsible for deleting textures
 }
 
-void SpriteObject::addAnimationFrame(aie::Texture * newFrame)
-{
-	m_animation.push_back(newFrame);
-}
-
-aie::Texture * SpriteObject::getCurrentAnimationFrame()
-{
-	return m_animation.back();		//Temp; There should ony be one texture
-}
+//void SpriteObject::addAnimationFrame(aie::Texture * newFrame)
+//{
+//	m_animation.push_back(newFrame);
+//}
+//
+//aie::Texture * SpriteObject::getCurrentAnimationFrame()
+//{
+//	return m_animation.back();		//Temp; There should ony be one texture
+//}
 
 void SpriteObject::draw(aie::Renderer2D * renderer)
 {
--- Kuno/SpriteObject.h
+++ Kuno/SpriteObject.h
@@ -10,21 +10,22 @@ namespace ai {
 class SpriteObject : public Agent
 {
 protected:
-	std::vector<aie::Texture*>	m_animation;
+	aie::Texture*	m_tex;					//Can also be a animation/tile set (use setUVRect())
 	pkr::Vector2	m_offset = { 0,0 };		//The texture offset from the BOTTOM_LEFT corner of the image
 	float			m_animationSpeed;		//ms
+	//std::vector<aie::Texture*>	m_animation;
 
 public:
 	SpriteObject(aie::Texture* texture);		//Texture is the first frame
 	virtual ~SpriteObject();
 
-	//Animation handling
-	void			addAnimationFrame(aie::Texture* newFrame);
-	aie::Texture*	getCurrentAnimationFrame();		//TBC
-	void			updateAnimationEngine();		//TBC
-
 	//Core
 	void			draw(aie::Renderer2D* renderer) override;
+
+	//Animation handling
+	//void			addAnimationFrame(aie::Texture* newFrame);
+	//aie::Texture*	getCurrentAnimationFrame();		//TBC
+	//void			updateAnimationEngine();		//TBC
 };
 
 }

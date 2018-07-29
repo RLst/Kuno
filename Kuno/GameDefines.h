9e3c4c8c5ec4d52516f54ea8b79b1e92b1177799
Tony Le <letony@icloud.com>
2018-07-29 11:49:13 +1000

    GameDefines cleanup

--- Kuno/GameDefines.h
+++ Kuno/GameDefines.h
@@ -4,18 +4,14 @@
 //#define SCREEN_WIDTH 1280
 //#define SCREEN_HEIGHT 720
 
-#define WORLD_WIDTH 10		//Tiles along X axis
-#define WORLD_DEPTH 10		//Tiles along Y axis
+#define WORLD_WIDTH			10		//Tiles along X axis
+#define WORLD_DEPTH			10		//Tiles along Y axis
 
-#define CART_TILE_WIDTH 128		//In this case, the tiles in this world (orthogonal)
-#define CART_TILE_HEIGHT 128		//is 128x128
-//#define CART_TILE_WIDTH -128		//In this case, the tiles in this world (orthogonal)
-//#define CART_TILE_HEIGHT -128		//is 128x128
+#define CART_TILE_WIDTH		128		//In this case, the tiles in this world (orthogonal)
+#define CART_TILE_HEIGHT	128		//is 128x128
 
-//#define ISO_TILE_WIDTH 100		//px; Width of isometric tile; Must change according to assets
-//#define ISO_TILE_HEIGHT 50		//px; Height of isometric tile; Must change according to assets
-#define ISO_TILE_WIDTH 255		//px; Width of isometric tile; Must change according to assets
-#define ISO_TILE_HEIGHT 148		//px; Height of isometric tile; Must change according to assets
+#define ISO_TILE_WIDTH		255		//px; Width of isometric tile; Must change according to assets
+#define ISO_TILE_HEIGHT		148		//px; Height of isometric tile; Must change according to assets
 const float TILE_RATIO = (float)ISO_TILE_WIDTH / (float)ISO_TILE_HEIGHT;	//Used for Iso<>Cart conversions
 
 namespace pkr {

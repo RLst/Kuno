aec906b67835d509c8c1f47750481369e2a2b910
Tony Le <letony@icloud.com>
2018-07-31 00:31:45 +1000

    Path; might not need this class

--- Kuno/Path.cpp
+++ Kuno/Path.cpp
@@ -1,24 +1,24 @@
-#include "Path.h"
-
-namespace pf {
-
-	Path::Path()
-	{
-		//Allocate 
-
-	}
-
-	Path::~Path()
-	{
-		//for (auto wayPoint : m_wayPoints) {
-		//	delete wayPoint;
-		//	wayPoint = nullptr;
-		//}
-	}
-
-	void Path::addWayPoint(const pkr::Vector2 wayPoint)
-	{
-		m_path.push_back(wayPoint);
-	}
-
-}
+//#include "Path.h"
+//
+//namespace pf {
+//
+//	Path::Path()
+//	{
+//		//Allocate 
+//
+//	}
+//
+//	Path::~Path()
+//	{
+//		//for (auto wayPoint : m_wayPoints) {
+//		//	delete wayPoint;
+//		//	wayPoint = nullptr;
+//		//}
+//	}
+//
+//	void Path::addWayPoint(const pkr::Vector2 wayPoint)
+//	{
+//		m_path.push_back(wayPoint);
+//	}
+//
+//}

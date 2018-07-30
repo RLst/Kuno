46e0c4bb450cc2c857856137b3e2e06af98e0036
Tony Le <letony@icloud.com>
2018-07-31 01:53:57 +1000

    Edge; renames, general clean Node; cPos and iPos (cartesian, isometric) positions now stored in node publically General clean, design, renames

--- Kuno/PF.cpp
+++ Kuno/PF.cpp
@@ -1,69 +1,162 @@
 #include "PF.h"
 
+//#include <list>
+//#include <stack>
+
 namespace pf {
 
-pf::Node * pf::Graph::addNode(pkr::Vector2 pos)
-{
-	//Create new node with set properties, push it onto graph node list and return
-	Node* newNode = new Node(pos);
-	m_nodes.push_back(newNode);
-	return newNode;
-}
-
-void pf::Graph::addConnection(Node * nodeFrom, Node * nodeTo, float cost)
-{
-	Node::connect(nodeFrom, nodeTo, cost);
-	//Edge* edge = new Edge(nodeTo, cost);			//Create new edge and connect to TARGET node and set costs
-	//nodeFrom->connections.push_back(edge);			//Connect edge to RECEIVING node
-}
-
-Node * Graph::findNode(pkr::Vector2 position, float searchRadius)
-{
-	for (auto node : m_nodes) {
-		//Return node that is within range of search position
-		if (pkr::Vector2::distance(node->pos_tmp, position) < searchRadius)
-			return node;
+	bool Graph::sortAscending(Node * a, Node * b) const
+	{
+		return a->gScore < b->gScore;
 	}
-}
 
-std::list<Node*>& Graph::findNodes(pkr::Vector2 position, float searchRadius)
-{
-	NodeList foundNodes;
-	for (auto node : m_nodes) {
-		//Push within range nodes onto list
-		if (pkr::Vector2::distance(node->pos_tmp, position) < searchRadius)
-			foundNodes.push_back(node);
-	}
-	return foundNodes;
-}
-
-NodeList Graph::AStarSearch(Node * startNode, Node * endNode)
-{
-	//Set all parents nodes to
-
-
-
-	return NodeList();
-}
-
-void pf::Graph::draw(aie::Renderer2D * renderer)
-{
-	//Draw edges on the bottom
-	for (auto node : m_nodes) {
-		for (auto it : node->connections)
+	Graph::~Graph()
+	{			//Delete all nodes
+		for (auto &node : m_nodes) 
 		{
-			Edge* edge = it;
-			renderer->setRenderColour(1, 1, 1);
-			renderer->drawLine(node->pos_tmp.x, node->pos_tmp.y, edge->connectTo->pos_tmp.x, edge->connectTo->pos_tmp.y);
+			delete node;
+			node = nullptr;
 		}
 	}
 
-	//Draw nodes on top
-	for (auto node : m_nodes) {
-		renderer->setRenderColour(1, 0.3f, 0);		//Required?
-		//renderer->drawBox(node->pos.x, node->pos.y, 10, 10);
-		renderer->drawCircle(node->pos_tmp.x, node->pos_tmp.y, 10);
+	pf::Node * pf::Graph::addNode(pkr::Vector2 pos)
+	{
+		//Create new node with set properties, push it onto graph node list and return
+		Node* newNode = new Node(pos);
+		m_nodes.push_back(newNode);
+		return newNode;
 	}
-}
+
+	void pf::Graph::addConnection(Node * nodeFrom, Node * nodeTo, float cost)
+	{
+		Node::connect(nodeFrom, nodeTo, cost);
+		//Edge* edge = new Edge(nodeTo, cost);			//Create new edge and connect to TARGET node and set costs
+		//nodeFrom->connections.push_back(edge);			//Connect edge to RECEIVING node
+	}
+
+	Node * Graph::findNode(pkr::Vector2 position, float searchRadius)
+	{
+		for (auto node : m_nodes) {
+			//Return node that is within range of search position
+			if (pkr::Vector2::distance(node->cPos, position) < searchRadius)
+				return node;
+		}
+	}
+
+	std::list<Node*>& Graph::findNodes(pkr::Vector2 position, float searchRadius)
+	{
+		NodeList foundNodes;
+		for (auto node : m_nodes) {
+			//Push within range nodes onto list
+			if (pkr::Vector2::distance(node->cPos, position) < searchRadius)
+				foundNodes.push_back(node);
+		}
+		return foundNodes;
+	}
+
+	Path Graph::getDjikstraPath(Node * startNode, Node * endNode) const
+	{
+	//	//Inits
+	//	Node*	currentNode = nullptr;
+
+	//	NodeList	openList;	//Let openList be a list of nodes
+	//	NodeList	closedList;	//Let closedList be a list of nodes
+
+	//	startNode->parent = nullptr;	//Set startnode to null
+	//	openList.push_back(startNode);	//Add startnode to openList
+
+	//	while (!openList.empty()) {
+
+	//		openList.sort(sortAscending(startNode, endNode));		//Sort openList by node's.gscore //(startNode, endNode)
+
+	//		//Remove current node from openlist
+	//		currentNode = openList.front();	
+	//		openList.pop_front();		
+
+	//		//End path found, break out of loop
+	//		if (currentNode == endNode)
+	//			break;
+
+	//		//Add currentNode to closedList (marking it as traversed)
+	//		closedList.push_back(currentNode);
+
+	//		//For all connection c in current node
+	//		for (auto edge : currentNode->connections) {
+	//			//Add c.target to openList if not in closedList
+	//			for (auto d : closedList) {
+	//				if (d == edge->target) {
+	//					//In closedList, do nothing?
+	//				}
+	//				else {
+	//					//Not in closedList so add to openList ?
+	//					openList.push_back(edge->target);
+	//				}
+	//			}
+	//			edge->target->gScore = currentNode->gScore + edge->cost;
+	//			edge->target->parent = currentNode;
+	//		}
+	//	}
+
+		//Calculate path, in this example as positions
+		//typedef std::stack<pkr::Vector2> Path;
+		Path path;
+		//auto currentNode = endNode;
+		//while (currentNode != nullptr) {
+		//	path.push(currentNode->pos_tmp);
+		//	currentNode = currentNode->parent;
+		//}
+		return path;
+	}
+
+	Path Graph::getAStarPath(Node * startNode, Node * endNode) const
+	{
+		////Set all parents nodes to
+
+		////Set all nodes to null..
+		////Set all gScores to infinity...
+
+		//int currentStep = 0;
+		//std::list<Node*> openList;				//Priority queue
+		//std::list<Node*> closedList;			//Traversed queue
+
+		////Set initial end node
+		//Node* endNode = nullptr;
+
+		////Set G score and parent
+		//startNode->gScore = 0;
+		//startNode->parent = nullptr;
+
+		////Push start node onto priority queue
+		//openList.push_back(startNode);
+
+		////While queue (priority) not empty
+		//while (!openList.empty()) {
+
+		//	//Get the current node off the end of the queue and remove it
+		//	//openList.
+		//}
+
+		return Path();
+	}
+
+	void Graph::draw(aie::Renderer2D * renderer)
+	{
+		//Draw edges on the bottom
+		for (auto node : m_nodes) {
+			for (auto it : node->connections)
+			{
+				Edge* edge = it;
+				renderer->setRenderColour(1, 1, 1);
+				renderer->drawLine(node->cPos.x, node->cPos.y, edge->target->cPos.x, edge->target->cPos.y, 2.0f, 0.2f);
+			}
+		}
+
+		//Draw nodes on top
+		for (auto node : m_nodes) {
+			renderer->setRenderColour(1, 0.3f, 0);		//Required?
+			//renderer->drawBox(node->pos.x, node->pos.y, 10, 10);
+			renderer->drawCircle(node->cPos.x, node->cPos.y, 0.1f);
+		}
+	}
 
 }
--- Kuno/PF.h
+++ Kuno/PF.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #include <pkr\Vector2.h>
+#include <Renderer2D.h>
 #include <vector>
 #include <list>
-#include <Renderer2D.h>
+#include <stack>
 
 //Pathfinding related methods and implementations
 namespace pf {
@@ -12,14 +13,14 @@ namespace pf {
 
 	struct Edge
 	{
-		Node*	connectTo;
+		Node*	target;
 		float	cost;			//The traversal cost to move through this edge
 
-		Edge() : connectTo(nullptr), cost(0) {}		//Default
+		Edge() : target(nullptr), cost(0) {}		//Default
 
-		Edge(Node* nodeTo) : connectTo(nodeTo), cost(0) {}		//Overload (zero cost)
+		Edge(Node* nodeTo) : target(nodeTo), cost(0) {}		//Overload (zero cost)
 
-		Edge(Node* nodeTo, float cost) : connectTo(nodeTo), cost(cost) {}		//Overload
+		Edge(Node* nodeTo, float cost) : target(nodeTo), cost(cost) {}		//Overload
 	};
 
 	struct Node
@@ -28,56 +29,51 @@ namespace pf {
 		float				gScore;
 		float				hScore;
 		float				fScore() const { return gScore + hScore; }
-		pkr::Vector2		pos_tmp;				//Might put this in Tile instead
+		pkr::Vector2		cPos;				//CARTESIAN WORLD COORDINATES
+		pkr::Vector2		iPos;				//ISOMETROC WORLD COORDINATES
 		std::vector<Edge*>	connections;
 
-		Node() : gScore(0), /*pos(0, 0),*/ parent(nullptr) {}		//Default
+		Node() : gScore(0), cPos(0, 0), parent(nullptr) {}									//Default
+		Node(pkr::Vector2 pos) : gScore(0), cPos(pos), parent(nullptr) {}					//Overload (nullptr parent)
+		Node(pkr::Vector2 pos, Node* parent) : gScore(0), cPos(pos), parent(parent) {}		//Overload
 
-		Node(pkr::Vector2 pos) : gScore(0), /*pos(pos),*/ parent(nullptr) {}		//Overload (nullptr parent)
-
-		Node(pkr::Vector2 pos, Node* parent) : gScore(0), /*pos(pos),*/ parent(parent) {}		//Overload
-
-		//Set connection (helper function?)
-		void static connect(Node *nodeFrom, Node *nodeTo, float cost) {
-			//Create new edge and connect to TARGET node and set costs
-			Edge* edge = new Edge(nodeTo, cost);
-			//Connect edge to RECEIVING node
-			nodeFrom->connections.push_back(edge);
+		void static connect(Node *nodeFrom, Node *nodeTo, float cost = 1.0f) {		//Set connection (helper function?)
+			Edge* edge = new Edge(nodeTo, cost);			//Create new edge and connect to TARGET node and set costs
+			nodeFrom->connections.push_back(edge);			//Connect edge to RECEIVING node
 		}
 	};
 
+	typedef std::vector<pkr::Vector2> Path;
 	typedef std::list<Node*> NodeList;
 
 	class Graph
 	{
+	private:
+		bool	sortAscending(Node* a, Node* b) const;		//Helper function for priority queue sorting
+
 	protected:
 		NodeList	m_nodes;		//***The graph's container of nodes; should I use list or vectors?
 
 	public:
 		Graph() {};
-		~Graph() {			//Delete all nodes
-			for (auto &node : m_nodes) {
-				delete node;
-				node = nullptr;
-			}
-		}
+		~Graph();
 
 		//Accessors
-		NodeList& getNodes() { return m_nodes; }
+		NodeList&	getNodes() { return m_nodes; }
 
-		Node*	addNode(pkr::Vector2 pos);
+		Node*		addNode(pkr::Vector2 pos);
 
 		//Remove the given node
-		void	removeNode(Node* node) { m_nodes.remove(node); }
-		void	addConnection(Node* nodeFrom, Node* nodeTo, float cost = 1.0f);
-		Node*	findNode(pkr::Vector2 position, float searchRadius = 0.0f);			//Return node that is at position or within range
-		NodeList& findNodes(pkr::Vector2 position, float searchRadius = 0.0f);	//Return a list of nodes that are within a certain search radius
+		void		removeNode(Node* node) { m_nodes.remove(node); }
+		void		addConnection(Node* nodeFrom, Node* nodeTo, float cost = 1.0f);
+		Node*		findNode(pkr::Vector2 position, float searchRadius = 0.0f);			//Return node that is at position or within range
+		NodeList&	findNodes(pkr::Vector2 position, float searchRadius = 0.0f);		//Return a list of nodes that are within a certain search radius
 
-		NodeList	AStarSearch(Node* startNode, Node* endNode);		//Returns A* path
-		NodeList	DjikstraSearch(Node* startNode, Node* endNode);		//Returns djikstra path
+		Path		getDjikstraPath(Node* startNode, Node* endNode) const;	
+		Path		getAStarPath(Node* startNode, Node* endNode) const;
 
 		//Cores
 		void	draw(aie::Renderer2D* renderer);
-		//void	findNodes(std::list<Node*> &list, pkr::Vector2 position, float distance);	//do I need this?
+
 	};
 }

8c0fce95191828085b1fb34220925116fffc7611
Tony Le <letony@icloud.com>
2018-08-02 09:00:44 +1000

    Condition; new conditions

--- Kuno/Condition.cpp
+++ Kuno/Condition.cpp
@@ -1,6 +1,31 @@
 #include "Condition.h"
+#include "Agent.h"
 
 namespace ai {
+	WithinRangeCondition::WithinRangeCondition(Agent * target, float range) :
+		m_target(target), m_range(range) {}
+	eResult WithinRangeCondition::execute(Agent * agent, float deltaTime)
+	{
+		if (pkr::Vector2::distance(agent->getPos(), m_target->getPos()) < m_range)
+			return SUCCESS;		//Target agent is within range
+		else
+			return FAILURE;
+	}
 
+	InViewCondition::InViewCondition(Agent * target, pf::Map * map) :
+		m_target(target), m_map(map) {}
+	eResult InViewCondition::execute(Agent * agent, float deltaTime)
+	{
+		//If target agent is within line of sight, within view and within range...
+		if (pkr::Vector2::angleBetween(agent->getFacing()
+			//// ???????????
+			// How do I access Character::getFacing() without having to rewrite the whole execute()???
 
+			return SUCCESS;		//Target agent is IN VIEW
+		}
+		else
+			return FAILURE;
+		
+	}
+	
 }
--- Kuno/Condition.h
+++ Kuno/Condition.h
@@ -1,7 +1,43 @@
 #pragma once
+#include "AI.h"
+
+namespace pf {
+	class Map;
+}
 
 namespace ai {
+	class Agent;
+	class Character;
 
+	class WithinRangeCondition : public iBehaviour 
+	{
+		//Checks if an agent is within range
+	private:
+		Agent *		m_target;
+		float		m_range;
+
+	public:
+		~WithinRangeCondition() override { delete m_target; }
+		WithinRangeCondition(Agent* target, float range);
+		eResult execute(Agent* agent, float deltaTime) override;
+	};
+
+	class InViewCondition : public iBehaviour
+	{
+		//Checks to see if an agent is within view of target agent
+		//Will have to 
+	private:
+		Agent *		m_target;
+		pf::Map*	m_map;
+
+		float		m_FOV = 80;					//degrees
+		float		m_sightDistance = 100;		//px?
+
+	public:
+		~InViewCondition() override { delete m_target; }
+		InViewCondition(Agent* target, pf::Map* map);
+		eResult execute(Agent* agent, float deltaTime) override;
+	};
 
 
 }

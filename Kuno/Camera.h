6b5f510163f3f6049a9a77256c0d95d1c3023228
Tony Le <letony@icloud.com>
2018-08-02 12:14:00 +1000

    Camera; fine tune zoom speed

--- Kuno/Camera.h
+++ Kuno/Camera.h
@@ -20,7 +20,7 @@ namespace util {
 		//Zoom
 		float			m_minZoom = 7.5f;
 		float			m_maxZoom = 0.5f;
-		float			m_zoomSpeedMultiplier = 3.0f;
+		float			m_zoomSpeedMultiplier = 12.0f;
 		float			m_lastScrollPos = 0;		//Holds the previous mouse wheel position (if you don't default this to zero then it'll start off fully zoomed in etc)
 
 		friend class KunoApp;		//For debugging

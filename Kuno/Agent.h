75d6300ea2af294410ca2493ee2812b49560fe87
Tony Le <letony@icloud.com>
2018-08-03 16:57:44 +1000

    Agent; correct indents

--- Kuno/Agent.h
+++ Kuno/Agent.h
@@ -25,22 +25,22 @@ namespace ai {
 		std::vector<iBehaviour*> m_behaviours;
 
 		//Some basic transformation data
-		float					m_maxForce;
-		//pkr::Vector2			m_force;
-		//pkr::Vector2			m_accel;
-		pkr::Vector2			m_vel;
-		pkr::Vector2			m_cPos;			//px; Cartesian
-		pkr::Vector2			m_iPos;			//px; Isometric
+		float				m_maxForce;
+		//pkr::Vector2		m_force;
+		//pkr::Vector2		m_accel;
+		pkr::Vector2		m_vel;
+		pkr::Vector2		m_cPos;			//px; Cartesian
+		pkr::Vector2		m_iPos;			//px; Isometric
 
 		//Circle agent
-		float					m_size;
-		pkr::Vector3			m_colour;
+		float				m_size;
+		pkr::Vector3		m_colour;
 
 		//Path Following
-		int						m_currentWaypointIndex;	//???Is this right?
+		int					m_currentWaypointIndex;	//???Is this right?
 		std::vector<pkr::Vector2*> m_currentPath;
-		float					m_waypointSearchRadius;
-		pkr::Vector2			pathFollowing();
+		float				m_waypointSearchRadius;
+		pkr::Vector2		pathFollowing();
 
 	public:
 		//Agent(const Agent &other);	//Copy
@@ -48,19 +48,19 @@ namespace ai {
 		Agent(float circleSize = 25.0f, const pkr::Vector3 &colour = { 0.75f, 0.75f, 0.75f }, const pkr::Vector2 &startingPos = { 300, 300 });
 
 		//Behaviours
-		void					addBehaviour(iBehaviour* behaviour);
+		void				addBehaviour(iBehaviour* behaviour);
 
 		//Translation
-		void					move(const pkr::Vector2 &lSpeed, float deltaTime);			//Linear move (instant vel/accel)
-		void					moveIso(const pkr::Vector2 &lSpeed, float deltaTime);		//Linear move in isometric
+		void				move(const pkr::Vector2 &lSpeed, float deltaTime);			//Linear move (instant vel/accel)
+		void				moveIso(const pkr::Vector2 &lSpeed, float deltaTime);		//Linear move in isometric
 
 		//State accessors
-		pkr::Vector2			getPos() const { return m_iPos; }
-		pkr::Vector2			getVel() const { return m_vel; }
+		pkr::Vector2		getPos() const { return m_iPos; }
+		pkr::Vector2		getVel() const { return m_vel; }
 
 		//Core
-		virtual void			draw(aie::Renderer2D* renderer);		//Agent drawn as a circle; this only runs if a sprite object does not override
-		virtual void			update(float deltaTime);
+		virtual void		update(float deltaTime);
+		virtual void		draw(aie::Renderer2D* renderer);		//Agent drawn as a circle; this only runs if a sprite object does not override
 	
 		//Allow access to parent methods (
 
@@ -70,10 +70,18 @@ namespace ai {
 	class Character : public Agent
 	{
 	private:
+		//Textures
+		aie::Texture*		m_tex;
+
+		//Extra translations
 		pkr::Vector2		m_facing;		//normalised vec; where the character is facing
 
 	public:
 		pkr::Vector2		getFacing() const { return m_facing; }
+
+		//Core
+		void				update(float deltaTime) override;
+		void				draw(aie::Renderer2D* renderer) override;
 	};
 
 }
